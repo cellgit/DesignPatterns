45e79496f51104cc52b4a163b40b0112506d4a33
cellgit <liuhongli2120@gmail.com>
2020-06-22 11:20:57 +0800

    init

---
+++ DesignPatterns/DesignPatterns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignPatterns
+//
+//  Created by 刘宏立 on 2020/6/22.
+//  Copyright © 2020 刘宏立. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
